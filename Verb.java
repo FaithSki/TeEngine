9904257452533eec185e685d50e2cb2b96eac370
JoeCleary <joseph.david.cleary@gmail.com>
2016-04-19 09:08:25 -0400

    Update Verb.java

--- Verb.java
+++ Verb.java
@@ -4,14 +4,22 @@ public class Verb {
 	public Item actsOn;
 	
 	public static void drop(Item dropItem, Room currentRoom){
-		if (dropItem.dropAble == true){
-			for (int count = 0; count < inventory.size();){
+		/*
+		if(dropItem.dropAble == true){
+			for(int count = 0; count < inventory.size();){
 				
 			}
 		}
 		else{
 			System.out.print("You'd rather not part with that.");
-		}
+		}*/
 		// wip, hope you can see what i was doing
+		//no idear wat youre doing wit the for loop so I'm gonna leave that, i'd think this is all you have to do(with errors)
+		if(dropItem.dropAble){
+			inventory.remove(dropItem);
+			currentRoom.items.add(dropItem);
+		}else
+			System.out.print("You'd rather not part with that.");
+		
 	}
 }
