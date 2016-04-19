1074d04b031c65965a88a261c703e1ff3ce7484b
FaithSki <ccvpieman@gmail.com>
2016-04-19 08:08:33 -0400

    Update Verb.java

--- Verb.java
+++ Verb.java
@@ -2,4 +2,16 @@ public class Verb {
 	public String name;
 	public static Verb allVerbs[];
 	public Item actsOn;
+	
+	public static void drop(Item dropItem, Room currentRoom){
+		if (dropItem.dropAble == true){
+			for (int count = 0; count < inventory.size();){
+				
+			}
+		}
+		else{
+			System.out.print("You'd rather not part with that.");
+		}
+		// wip, hope you can see what i was doing
+	}
 }
