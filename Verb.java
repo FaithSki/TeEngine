571a3d55efd3fc660735077987e051c28b4bbb2b
FaithSki <ccvpieman@gmail.com>
2016-04-19 13:09:02 -0400

    Update Verb.java

--- Verb.java
+++ Verb.java
@@ -4,22 +4,27 @@ public class Verb {
 	public Item actsOn;
 	
 	public static void drop(Item dropItem, Room currentRoom){
-		/*
-		if(dropItem.dropAble == true){
-			for(int count = 0; count < inventory.size();){
-				
-			}
-		}
-		else{
-			System.out.print("You'd rather not part with that.");
-		}*/
-		// wip, hope you can see what i was doing
-		//no idear wat youre doing wit the for loop so I'm gonna leave that, i'd think this is all you have to do(with errors)
+		// I didn't realize that you can just specify the name of the object when removing it from an ArrayList;
+		// the for loop was for going through each element in the ArrayList to see if it matched dropItem ^^
 		if(dropItem.dropAble){
-			inventory.remove(dropItem);
+			PlayerInfo.inventory.remove(dropItem);
 			currentRoom.items.add(dropItem);
+			System.out.println("You drop the " + dropItem.itemName() + ".");
 		}else
 			System.out.print("You'd rather not part with that.");
-		
+	}
+	public static void take(Item takeItem, Room currentRoom){
+		if(takeItem.pickupAble){
+			PlayerInfo.inventory.add(takeItem);
+			currentRoom.items.remove(takeItem);
+			System.out.println("You take the " + takeItem.itemName() + ".");
+		}else
+			System.out.println("It won't budge.");
+		// once we have our demo done, we should figure out how to have it so items can override these "you can't do" messages.
+		// so, like, idk; "It won't budge" is fine for, like, a boulder, but it'd be nice to add extra flavor for things that have
+		// other reasons as to why you can't/won't take it, drop it, etc.
+	}
+	public static void examine(Item examineItem, Room currentRoom){
+		System.out.println(examineItem.itemDescription());
 	}
 }
