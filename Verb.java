ebf166e195c4dfc642b102423a02434f15eeb16e
JoeCleary <joseph.david.cleary@gmail.com>
2016-04-20 09:02:52 -0400

    Update Verb.java

--- Verb.java
+++ Verb.java
@@ -6,6 +6,7 @@ public class Verb {
 	public static void drop(Item dropItem, Room currentRoom){
 		// I didn't realize that you can just specify the name of the object when removing it from an ArrayList;
 		// the for loop was for going through each element in the ArrayList to see if it matched dropItem ^^
+		// I'm just assuming it works
 		if(dropItem.dropAble){
 			PlayerInfo.inventory.remove(dropItem);
 			currentRoom.items.add(dropItem);
@@ -13,20 +14,25 @@ public class Verb {
 		}else
 			System.out.print("You'd rather not part with that.");
 	}
+	
 	public static void take(Item takeItem, Room currentRoom){
 		if(takeItem.pickupAble){
 			PlayerInfo.inventory.add(takeItem);
 			currentRoom.items.remove(takeItem);
 			System.out.println("You take the " + takeItem.itemName() + ".");
 		}else
-			System.out.println("It won't budge.");
+			System.out.println(takeItem.cantInteractText);
 		// once we have our demo done, we should figure out how to have it so items can override these "you can't do" messages.
 		// so, like, idk; "It won't budge" is fine for, like, a boulder, but it'd be nice to add extra flavor for things that have
 		// other reasons as to why you can't/won't take it, drop it, etc.
+		
+		//with this we'll probably run into some exception eventually but it should work for objects that you cant pick up because of one un-changing thing
 	}
-	public static void examine(Item examineItem, Room currentRoom){
+	
+	public static void examine(Item examineItem){
 		System.out.println(examineItem.itemDescription());
 	}
+	
 	public static void look(Room currentRoom){
 		System.out.println(currentRoom.roomDesc());
 	}
