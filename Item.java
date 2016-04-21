c0da96976db2c2590e57be94bd078b6694e2b17a
FaithSki <ccvpieman@gmail.com>
2016-04-21 08:08:31 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,40 +1,39 @@
-import java.util.ArrayList;
-
-
-public class Item {
-	String itemName; // What the item is referenced as.
-	String itemDescription; // What the player sees upon Examining the item.
-	boolean pickupAble; // Can the item be picked up?
-	boolean useAble; // Can the item be interacted with? (Use, Combine, Open, etc)
-	boolean dropAble; // Can the item be dropped into a room?
-	Room location;
-	public Verb[] possibleActions;
-	public String cantInteractText;
-	
+ import java.util.ArrayList;
+ 
+ 
+ public class Item {
+ 	String itemName; // What the item is referenced as.
+ 	String itemDescription; // What the player sees upon Examining the item.
+ 	boolean pickupAble; // Can the item be picked up?
+ 	boolean useAble; // Can the item be interacted with? (Use, Combine, Open, etc)
+ 	boolean dropAble; // Can the item be dropped into a room?
+ 	Room location;
+ 	public Verb[] possibleActions;
+	//public String cantInteractText;
+	// I'd rather not do it like that, cuz we'd want a different message for being unable to pickup, unable to drop, unable to use, etc
 	public static Item[] allItems;
-	
-	
-	Item(String inputName, String inputDescription, boolean inputPickupAble, boolean inputUseAble, boolean inputDropAble){
-		itemName = inputName;
-		itemDescription = inputDescription;
-		pickupAble = inputPickupAble;
-		useAble = inputUseAble;
-		dropAble = inputDropAble;
-	}
-	// Following methods all just retrieve variables
-	public String itemName(){
-		return itemName;
-	}
-	public String itemDescription(){
-		return itemDescription;
-	}
-	public boolean pickupAble(){
-		return pickupAble;
-	}
-	public boolean useAble(){
-		return useAble;
-	}
-	public boolean dropAble(){
-		return dropAble;
-	}
-}
+ 	
+ 	Item(String inputName, String inputDescription, boolean inputPickupAble, boolean inputUseAble, boolean inputDropAble){
+ 		itemName = inputName;
+ 		itemDescription = inputDescription;
+ 		pickupAble = inputPickupAble;
+ 		useAble = inputUseAble;
+ 		dropAble = inputDropAble;
+ 	}
+ 	// Following methods all just retrieve variables
+ 	public String itemName(){
+ 		return itemName;
+ 	}
+ 	public String itemDescription(){
+ 		return itemDescription;
+ 	}
+ 	public boolean pickupAble(){
+ 		return pickupAble;
+ 	}
+ 	public boolean useAble(){
+ 		return useAble;
+ 	}
+ 	public boolean dropAble(){
+ 		return dropAble;
+ 	}
+ }
