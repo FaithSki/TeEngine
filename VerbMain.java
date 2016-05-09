751d765ea8f2bd37fc4b37cdf9f9943072ae1fae
FaithSki <ccvpieman@gmail.com>
2016-05-09 08:07:50 -0400

    Add files via upload

---
+++ VerbMain.java
@@ -0,0 +1,9 @@
+public class VerbMain {//this is how I think the verb class should be made, Im not sure if they should be public(call from everywhere) or private(call from a central method on the Verb class
+
+	public static String allVerbs[];
+	
+	public static Verb findVerb(String something){
+		// <code to find verb>
+		return new Verb.look();
+	}
+}
