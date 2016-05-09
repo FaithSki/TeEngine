34fc0c480bb81bde0cccc99adebbe17001aba662
FaithSki <ccvpieman@gmail.com>
2016-05-08 22:22:30 -0400

    Update PlayerInfo.java

--- PlayerInfo.java
+++ PlayerInfo.java
@@ -2,4 +2,5 @@ import java.util.ArrayList;
 
 public class PlayerInfo {
 	public static ArrayList<Item> inventory = new ArrayList<Item>();
+	public static Room playerRoom = new Room("","","",null,null);
 }
