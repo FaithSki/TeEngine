a899db2b8ecb82076ebc72f3e7f0464af65f473e
FaithSki <ccvpieman@gmail.com>
2016-05-09 08:06:25 -0400

    Update Verb.java
    pls no bully

--- Verb.java
+++ Verb.java
@@ -1,30 +1,31 @@
-public class Verb {//this is how I think the verb class should be made, Im not sure if they should be public(call from everywhere) or private(call from a central method on the Verb class
-	public static String name;
-	public static String format;
-	public static String allVerbs[];
-	
-	public Verb findVerb(String something){
-		return new look();
-	}
+
+public abstract class Verb {
+	public String name;
+	public String format;
+	abstract void action();
 	
 	public class SomeVerb extends Verb {
-		SomeVerb(){
-			
+		public void action(){
+			// this does nothing
 		}
 	}
 	
-	private class take extends Verb {
+	public class take extends Verb {
 		public String name = "take";
 		public String format = "take (Item)"; // a central method would replace any (Item)s and do something with it to make it work, something would look for items in those (Item) slots
 		//take(Item toTake){
 		//	PlayerInfo.inventory.add(toTake);
 		//}
+		public void action() {
+			PlayerInfo.inventory.add(verbItem);
+			PlayerInfo.playerRoom.items.remove(verbItem);
+			System.out.println("You take the " + verbItem.itemName);
+		}
 	}
 	
-	private static class look extends Verb {
+	public static class look extends Verb {
 		public static String name = "look";
 		public static String format = "look";
-		
 		public void action(){
 			System.out.println(PlayerInfo.playerRoom.roomDesc);
 		}
