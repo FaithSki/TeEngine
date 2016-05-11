dde120870ffb17657e68cdc83ea7588b6776502e
FaithSki <ccvpieman@gmail.com>
2016-05-11 08:02:39 -0400

    Update VerbMain.java

--- VerbMain.java
+++ VerbMain.java
@@ -2,8 +2,14 @@ public class VerbMain {//this is how I think the verb class should be made, Im n
 
 	public static String allVerbs[];
 	
-	public static Verb findVerb(String something){
+	public static Verb findVerb(String inputVerb){
 		// <code to find verb>
+		
 		return new Verb.look();
+	}
+	
+	private static void constructVerbList(){
+		allVerbs[0] = "look";allVerbs[1] = "examine";allVerbs[2] = "take";
+		// is this how you want to handle the allVerbs and allItems arrays?
 	}
 }
