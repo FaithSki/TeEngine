700890e18fe203b7c2060fc37dead29322e3b5a9
JoeCleary <joseph.david.cleary@gmail.com>
2016-05-11 09:09:20 -0400

    Update Verb.java

--- Verb.java
+++ Verb.java
@@ -1,19 +1,16 @@
-public abstract class Verb {
+import java.util.ArrayList;
+public class Verb {
 	public String name;
 	public String format;
-	abstract void action();
+	public static ArrayList<Verb> allVerbs = new ArrayList<Verb>();
 	
-	public class SomeVerb extends Verb {
-		public void action(){
-			// this does nothing
-		}
+	Verb(){
+		allVerbs.add(this); //I'm hoping that the constructor extends to every verb, then we dont have to worry about manually adding them, whenever the instance of it is made then it will automatically be added
 	}
 	
-	public class take extends Verb {
+	public static class take extends Verb {
 		public String name = "take", format[] = {"take","(Item)"}; // a central method would replace any (Item)s and do something with it to make it work, something would look for items in those (Item) slots
-		//take(Item toTake){
-		//	PlayerInfo.inventory.add(toTake);
-		//}
+		
 		public void action(Item takeItem) {
 			if(takeItem.location == PlayerInfo.playerRoom && takeItem.visible && takeItem.pickupAble){
 				PlayerInfo.inventory.add(takeItem);
@@ -22,13 +19,11 @@ public abstract class Verb {
 			}else
 				System.out.println(takeItem.cantPickUpMessage);
 		}
-		public void action(){
-			// this does nothing
-		}
 	}
 	
 	public static class look extends Verb {
 		public static String name = "look", format[] = {"look"};
+		
 		public void action(){
 			System.out.println(PlayerInfo.playerRoom.roomDesc);
 		}
@@ -36,14 +31,12 @@ public abstract class Verb {
 	
 	public static class examine extends Verb {
 		public String name = "examine", format[] = {"examine","(Item)"};
+		
 		public void action(Item examineItem) {
 			if (((examineItem.location == PlayerInfo.playerRoom) || (PlayerInfo.isItemOwned(examineItem))) && examineItem.visible){
 				System.out.println(examineItem.itemDescription);
 			}
 			System.out.println("You can't see anything like that.");
-		}
-		public void action() {
-			// this does nothing
 		}
 	}
 }
