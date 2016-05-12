ecc59a0d15f72cee8903a26542347c0a8f02dbf5
FaithSki <ccvpieman@gmail.com>
2016-05-12 13:04:17 -0400

    Update Exit.java

--- Exit.java
+++ Exit.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class Exit {
 	
 	String room1; // One of the two rooms that the Exit leads between; this room should be encountered before room2
@@ -9,7 +10,9 @@ public class Exit {
 	// not neccessary at all.
 	String[] room1Names; // What the Exit is called in room1; should be cardinal directions, if possible (n & north, se & southeast, etc)
 	String[] room2Names; // What the Exit is called in room2; should be cardinal directions, if possible (n & north, se & southeast, etc)
+	String[] generalName; 
 	boolean locked; // Can the Player travel through the Exit?
+	public static ArrayList<Exit> allExits = new ArrayList<Exit>();
 	
 	Exit(String inputRoom1, String inputRoom2, String[] inputRoom1Names, String[] inputRoom2Names, boolean inputLocked){
 		room1 = inputRoom1;
@@ -17,24 +20,39 @@ public class Exit {
  		room1Names = inputRoom1Names;
  		room2Names = inputRoom2Names;
  		locked = inputLocked;
+ 		allExits.add(this);
  	}
 	
 	public String room1(){
 		return room1;
 	}
+	
 	public String room2(){
 		return room2;
 	}
+	
 	public String room1Names(int index){
 		return room1Names[index];
 	}
+	
 	public String room2Names(int index){
 		return room2Names[index];
 	}
+	
 	public boolean locked(){
 		return locked;
 	}
+	
 	public void changeLocked(boolean toggle){
 		locked = toggle;
+	}
+	
+	public boolean isExit(Exit toCheck){
+		boolean is = true;
+		for(Exit exit : allExits);
+		
+			
+		
+			
 	}
 }
