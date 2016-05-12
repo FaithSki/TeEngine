04834fba1c5ff363ab339f5d5be9dd1ad726ffec
FaithSki <ccvpieman@gmail.com>
2016-05-12 13:06:30 -0400

    Update Item.java
    you accidentally wrote over Item with Exit. threw the push you put on this on to Exit, and restored the last Item version you made.
    <3

--- Item.java
+++ Item.java
@@ -1,58 +1,77 @@
 import java.util.ArrayList;
-public class Exit {
-	
-	String room1; // One of the two rooms that the Exit leads between; this room should be encountered before room2
-	String room2; // One of the two rooms that the Exit leads between; this room should be encountered after room1
-	// [room1] <---> (exit) <---> [room2]
-	// in room 1, exit is called "east" (room1Names = "east, e")
-	// in room 2, exit is called "west" (room2Names = "west, w")
-	// the player would, organizationally, have to go through room 1 before they've been in room 2, but that's totally 
-	// not neccessary at all.
-	String[] room1Names; // What the Exit is called in room1; should be cardinal directions, if possible (n & north, se & southeast, etc)
-	String[] room2Names; // What the Exit is called in room2; should be cardinal directions, if possible (n & north, se & southeast, etc)
-	String[] generalName; 
-	boolean locked; // Can the Player travel through the Exit?
-	public static ArrayList<Exit> allExits = new ArrayList<Exit>();
-	
-	Exit(String inputRoom1, String inputRoom2, String[] inputRoom1Names, String[] inputRoom2Names, boolean inputLocked){
-		room1 = inputRoom1;
- 		room2 = inputRoom2;
- 		room1Names = inputRoom1Names;
- 		room2Names = inputRoom2Names;
- 		locked = inputLocked;
- 		allExits.add(this);
+ 
+ 
+ public class Item {
+ 	String itemName[]; // What the item is referenced as.
+ 	String itemDescription; // What the player sees upon Examining the item.
+ 	boolean pickupAble; // Can the item be picked up?
+ 	boolean useAble; // Can the item be interacted with? (Use, Combine, Open, etc)
+ 	boolean dropAble; // Can the item be dropped into a room?
+ 	boolean visible; // Can the item be seen when the room is examined?
+ 	public Room location;
+ 	public Verb[] possibleActions;
+ 	public String cantPickUpMessage;
+ 	public String cantDropMessage;
+ 	public String cantUseMessage; // "use" is still super broad, but this should be fine for our demo & stuff
+ 	public static ArrayList<Item> allItems = new ArrayList<Item>();
+ 	
+ 	Item(String inputName, String inputDescription, boolean inputPickupAble, boolean inputUseAble, boolean inputDropAble, boolean inputVisible, Verb inputPossibleActions[], String inputCantPickUp){
+ 		itemName = inputName;
+ 		itemDescription = inputDescription;
+ 		pickupAble = inputPickupAble;
+ 		useAble = inputUseAble;
+ 		dropAble = inputDropAble;
+ 		visible = inputVisible;
+ 		possibleActions = inputPossibleActions;
+ 		cantPickUpMessage = inputCantPickUp;
+ 		allItems.add(this);
  	}
-	
-	public String room1(){
-		return room1;
-	}
-	
-	public String room2(){
-		return room2;
-	}
-	
-	public String room1Names(int index){
-		return room1Names[index];
-	}
-	
-	public String room2Names(int index){
-		return room2Names[index];
-	}
-	
-	public boolean locked(){
-		return locked;
-	}
-	
-	public void changeLocked(boolean toggle){
-		locked = toggle;
-	}
-	
-	public boolean isExit(Exit toCheck){
-		boolean is = true;
-		for(Exit exit : allExits);
-		
-			
-		
-			
-	}
-}
+ 	
+ 	public boolean isActionPossible(Verb checkVerb){
+ 		for(Verb action : possibleActions)
+ 			if(action == checkVerb) // must point to the same instance, look into this later
+ 				return true;
+ 		return false;
+ 	}
+ 	
+ 	public static boolean isItem(String word){
+ 		for(Item currentItem : allItems)
+ 			if(word.equalsIgnoreCase(currentItem.itemName))
+ 				return true;
+ 		return false;
+ 	}
+ 	
+ 	public static boolean isItem(String firstWord, String secondWord){
+ 		for(Item currentItem : allItems)
+ 			if(word.equalsIgnoreCase(currentItem.itemName))
+ 				return true;
+ 		return false;
+ 	}
+ 	
+ 	public static Item getItem(String word){
+ 		for(Item currentItem : allItems)
+ 			if(word.equalsIgnoreCase(currentItem.itemName))
+ 				return currentItem;
+ 		return null;
+ 	}
+ 	
+ 	// Following methods all just retrieve variables
+ 	public String itemName(){
+ 		return itemName;
+ 	}
+ 	public String itemDescription(){
+ 		return itemDescription;
+ 	}
+ 	public boolean pickupAble(){
+ 		return pickupAble;
+ 	}
+ 	public boolean useAble(){
+ 		return useAble;
+ 	}
+ 	public boolean dropAble(){
+ 		return dropAble;
+ 	}
+ 	public boolean visible(){
+ 		return visible;
+ 	}
+ }
