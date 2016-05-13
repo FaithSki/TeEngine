4d19760ada1b8388e6e4ddf52f71a234ba6cbf06
JoeCleary <joseph.david.cleary@gmail.com>
2016-05-13 09:08:42 -0400

    Update Verb.java

--- Verb.java
+++ Verb.java
@@ -8,6 +8,10 @@ public class Verb {
 		allVerbs.add(this); //I'm hoping that the constructor extends to every verb, then we dont have to worry about manually adding them, whenever the instance of it is made then it will automatically be added
 	}
 	
+	public static void executeAction(Item[] items, String[] verbStrings, Exit[] exits){
+		//for(int i = 0;i <= items.length;i++)
+	}
+	
 	public static class take extends Verb {
 		public String name = "take", format[] = {"take","(Item)"}; // a central method would replace any (Item)s and do something with it to make it work, something would look for items in those (Item) slots
 		
@@ -39,15 +43,3 @@ public class Verb {
 			System.out.println("You can't see anything like that.");
 		}
 	}
-	
-	public static class examine extends Verb {
-		public String name = "examine", format[] = {"examine","(Item)"};
-		
-		public void action(Item examineItem) {
-			if (((examineItem.location == PlayerInfo.playerRoom) || (PlayerInfo.isItemOwned(examineItem))) && examineItem.visible){
-				System.out.println(examineItem.itemDescription);
-			}
-			System.out.println("You can't see anything like that.");
-		}
-	}
-}
