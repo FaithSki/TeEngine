fbaee511b5decf945048a9986b2c1c322ea45e27
JoeCleary <joseph.david.cleary@gmail.com>
2016-05-17 08:57:52 -0400

    Update Verb.java

--- Verb.java
+++ Verb.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 public class Verb {
 	public String name;
 	public String format;
@@ -8,41 +9,51 @@ public class Verb {
 		allVerbs.add(this); //I'm hoping that the constructor extends to every verb, then we dont have to worry about manually adding them, whenever the instance of it is made then it will automatically be added
 	}
 	
-	public static void findAndExecuteAction(Item[] items, String[] verbStrings, Exit[] exits){
+	public static void findAndExecuteAction(Item[] items, String[] verbStrings){
 		System.out.println(items[0]);
 		System.out.println(verbStrings[0]);
-		System.out.println(exits[0]);
-		//for(int i = 0;i <= items.length;i++)
+		for(Verb toExecute : allVerbs)
+			if(Arrays.deepEquals(toExecute.format, verbStrings))
+				// i still have to look into this method
 	}
 	
 	public static class take extends Verb {
-		public String name = "take", format[] = {"take","(Item)"}; // a central method would replace any (Item)s and do something with it to make it work, something would look for items in those (Item) slots
+		public String name = "take", format[] = {"take",null}; // a central method would replace any (Item)s and do something with it to make it work, something would look for items in those (Item) slots
 		
-		public void action(Item takeItem) {
-			if(takeItem.location == PlayerInfo.playerRoom && takeItem.visible && takeItem.pickupAble){
-				PlayerInfo.inventory.add(takeItem);
-				PlayerInfo.playerRoom.items.remove(takeItem);
-				System.out.println("You take the " + takeItem.itemName + ".");
+		public void action(Item[] items, String[] verbStrings, Exit[] exits) {
+			if(items[1].location == PlayerInfo.playerRoom && items[1].visible && items[1].pickupAble){
+				PlayerInfo.inventory.add(items[1]);
+				PlayerInfo.playerRoom.items.remove(items[1]);
+				System.out.println("You take the " + items[1].itemName + ".");
 			}else
-				System.out.println(takeItem.cantPickUpMessage);
+				System.out.println(items[1].cantPickUpMessage);
 		}
 	}
 	
 	public static class look extends Verb {
 		public static String name = "look", format[] = {"look"};
 		
-		public void action(){
+		public void action(Item[] items, String[] verbStrings){
 			System.out.println(PlayerInfo.playerRoom.roomDesc);
 		}
 	}
 	
 	public static class examine extends Verb {
-		public String name = "examine", format[] = {"examine","(Item)"};
+		public String name = "examine", format[] = {"examine",null};
 		
-		public void action(Item examineItem) {
-			if (((examineItem.location == PlayerInfo.playerRoom) || (PlayerInfo.isItemOwned(examineItem))) && examineItem.visible){
-				System.out.println(examineItem.itemDescription);
+		public void action(Item[] items, String[] verbStrings) {
+			if (((items[1].location == PlayerInfo.playerRoom) || (PlayerInfo.isItemOwned(items[1]))) && items[1].visible){
+				System.out.println(items[1].itemDescription);
 			}
+			System.out.println("You can't see anything like that.");
+		}
+	}
+	
+	public static class north extends Verb {
+		public String name = "north", format[] = {"north"};
+		
+		public void action(Item[] items, String[] verbStrings) {
+			
 			System.out.println("You can't see anything like that.");
 		}
 	}
@@ -50,7 +61,7 @@ public class Verb {
 	public static class help extends Verb {
 		public String name = "help", format[] = {"help"};
 		
-		public void action() {
+		public void action(Item[] items, String[] verbStrings) {
 			System.out.println("This is a text-based adventure, inspired by Infocom interactive fiction games.");
 			System.out.println("You may perform actions by inputting verbs, or verbs followed by nouns.");
 			System.out.println("For example; 'look', 'examine box', 'take key', etc.");
