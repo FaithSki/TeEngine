3dac6935e9792d7f3ee2d1dc11b92265ff830804
JoeCleary <joseph.david.cleary@gmail.com>
2016-05-17 08:58:41 -0400

    Update Room.java

--- Room.java
+++ Room.java
@@ -4,15 +4,21 @@ public class Room {
 	String roomName; // What the room is referenced as.
 	String roomDesc; // The description of the room, given upon first entering the room or using the "Look" verb.
 	String roomShortDesc; // The abridged description of the room, given upon entering the room for the second time and onwards.
-	ArrayList exits; // Directions and objects leading out of the room. (N, E, W, S, Up, Down, Secret Door, etc)
+	Exit north;
+	Exit south;
+	Exit east;
+	Exit west;
 	ArrayList items; // Items placed in the room.
 	boolean visited = false; // Has the player been inside of the room before?
 	
-	Room(String inputName, String inputDesc, String inputShortDesc, ArrayList inputExits, ArrayList inputItems){
+	Room(String inputName, String inputDesc, String inputShortDesc,Exit inputNorth, Exit inputSouth, Exit inputEast, Exit inputWest, ArrayList inputItems){
 		roomName = inputName;
 		roomDesc = inputDesc;
 		roomShortDesc = inputShortDesc;
-		exits = inputExits;
+		north = inputNorth;
+		south = inputSouth;
+		east = inputEast;
+		west = inputWest;
 		items = inputItems;
 	}
 	// Following methods all just retrieve variables
@@ -24,9 +30,6 @@ public class Room {
 	}
 	public String roomShortDesc(){
 		return roomShortDesc;
-	}
-	public ArrayList exits(){
-		return exits;
 	}
 	public ArrayList items(){
 		return items;
