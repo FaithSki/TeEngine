54a7eed9e539745e70acf34b34ebf64f35e115cd
JoeCleary <joseph.david.cleary@gmail.com>
2016-05-18 09:07:41 -0400

    Update Verb.java

--- Verb.java
+++ Verb.java
@@ -16,19 +16,19 @@ public class Verb {
 		for(Verb toExecute : allVerbs){
 			if(Arrays.deepEquals(toExecute.format, verbStrings)){
 				System.out.println("yeet");
+				toExecute.action(items); // some weird stuff happening here, not sure what exactly to do
 			}
 			else{
 				System.out.println("you goofed");
 			}
 		}
-		
 				// i still have to look into this method
 	}
 	
 	public static class take extends Verb {
 		public String name = "take", format[] = {"take",null}; // a central method would replace any (Item)s and do something with it to make it work, something would look for items in those (Item) slots
 		
-		public void action(Item[] items, String[] verbStrings, Exit[] exits) {
+		public static void action(Item[] items) {
 			if(items[1].location == PlayerInfo.playerRoom && items[1].visible && items[1].pickupAble){
 				PlayerInfo.inventory.add(items[1]);
 				PlayerInfo.playerRoom.items.remove(items[1]);
@@ -45,7 +45,7 @@ public class Verb {
 		}
 		public static String name = "look", format[] = {null,"look"};
 		
-		public void action(Item[] items, String[] verbStrings){
+		public static void action(Item[] items){
 			System.out.println(PlayerInfo.playerRoom.roomDesc);
 		}
 	}
@@ -53,7 +53,7 @@ public class Verb {
 	public static class examine extends Verb {
 		public String name = "examine", format[] = {"examine",null};
 		
-		public void action(Item[] items, String[] verbStrings) {
+		public static void action(Item[] items) {
 			if (((items[1].location == PlayerInfo.playerRoom) || (PlayerInfo.isItemOwned(items[1]))) && items[1].visible){
 				System.out.println(items[1].itemDescription);
 			}
@@ -64,8 +64,7 @@ public class Verb {
 	public static class north extends Verb {
 		public String name = "north", format[] = {null,"north"};
 		
-		public void action(Item[] items, String[] verbStrings) {
-			
+		public static void action(Item[] items) {
 			System.out.println("You can't see anything like that.");
 		}
 	}
@@ -73,15 +72,23 @@ public class Verb {
 	public static class help extends Verb {
 		public String name = "help", format[] = {null,"help"};
 		
-		public void action(Item[] items, String[] verbStrings) {
+		public static void action(Item[] items) {
 			System.out.println("This is a text-based adventure, inspired by Infocom interactive fiction games.");
 			System.out.println("You may perform actions by inputting verbs, or verbs followed by nouns.");
-			System.out.println("For example; 'look', 'examine box', 'take key', etc.");
+			System.out.println("For example; \"look\", \"examine box\", \"take key\", etc.");
 			System.out.println("Additionally, you may navigate between rooms using cardinal directions.");
-			System.out.println("'North', or simply 'n', can be inputted to travel through an exit to the north.");
+			System.out.println("\"North\", or simply \"n\", can be inputted to travel through an exit to the north.");
 			System.out.println();
 			System.out.println("Have fun!");
 			System.out.println();
 			}
+	}
+	
+	public static class test extends Verb {
+		public String name = "test", format[] = {"test"};
+		
+		public static void action(Item[] items) {
+			System.out.println("test");
+		}
 	}
 }
