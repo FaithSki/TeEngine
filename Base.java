62b16b5bfa8b340f9f8808dc115f0a1908333271
FaithSki <ccvpieman@gmail.com>
2016-05-24 23:14:23 -0400

    everything is broken

--- Base.java
+++ Base.java
@@ -57,7 +57,7 @@ public class Base {
 		Item present = new Item(presentName,"A plain cardboard box, featureless aside from the little plastic blue bow taped on to the top. A note is taped to the side.",true,false,false,true,presentActions,"You don't forsee much of a need for a cardboard box. But what's inside?",null,null);
 		Verb[] noteActions = {null};
 		String[] noteName = {"Note"};
-		Item note = new Item(noteName,null,false,false,false,true,keyCardActions,"It's stuck to the box.. though, you can't see anything adhering it to the cardboard.",null,null);// we will put the stuff for picture here
+		Item note = new Item(noteName,null,false,false,false,true,noteActions,"It's stuck to the box.. though, you can't see anything adhering it to the cardboard.",null,null);// we will put the stuff for picture here
 		// The note should open a graphic. Just put a weird, vague, cryptic message on it, like it's from a mysterious benefactor/creepy-weirdo. The box will contain the keycard.
 		Verb[] generalActions = {null};
 		Item blank = new Item(null,"",false,false,false,false,generalActions,"",null,null);
@@ -66,7 +66,7 @@ public class Base {
 		/*Exit constructor takes, in this order:
 		 * Room 1 (Room)
 		 * Room 2 (Room)
-		 * Name in Room 1 (String)
+		 * Name in Room 1 (String)e
 		 * Name in Room 2 (String)
 		 * Description of Exiting Room 1 (String)
 		 * Description of Exiting Room 2 (String)
@@ -89,10 +89,16 @@ public class Base {
 		 */
 		
 		Room startApartment = new Room("APARTMENT","You've awoken in the dim light of this small, one-room apartment. The small bed you awoke upon sits in the back corner, facing an old television that's been\npushed up against the wall. Drawing your gaze from the TV is a dirty old window, softly glowing with the slight blue light that illuminates the room. Apparently, the\nlight bulb in the ceiling is failing at its job. A faux-wood door is set into the western wall of the room. The rest of the apartment is entirely featureless,\naside from the occasional stain in the carpet and tear in the plain wallpaper. You can hear rain outside.","The dirty old apartment you awoke in. Its only exit is through a door to the west. A TV sits below a window, in front of the bed.\nThe ceiling light is off. You can hear rain outside.",null,null,null,null,items);
+		bed.setLocation(startApartment);
+		television.setLocation(startApartment);
+		window.setLocation(startApartment);
+		present.setLocation(startApartment);
+		keyCard.setLocation(startApartment);
+		note.setLocation(startApartment);
 		resetRoomArrayLists();
-		items.add(window);
 		Room commonRoom = new Room("COMMON ROOM","This room is long; more of a hallway with furniture, you figure. Doors line the eastern wall, at least a dozen of them, all identical to the one leading into\n \"your\" apartment, aside from their lack of any sort of handle. Aside from the doors, this room features clusters of old mis-matched plastic and metal chairs, each one\ncushioned and surrounding a stainless steel table. A small kitchenette sits on the western wall, along with a rather nondescript door. To the north lies a curtained\nwindow. You can hear rain outside.","A long common room, dotted with decaying furniture, apartment doors, and a kitchenette. The exit lies to the west. To the north, a window. The apartment you awoke\nin is to the east. You can hear rain outside.",null,null,null,null,items);
-		
+		Item window2 = window;
+		window2.setLocation(commonRoom);
 		
 		Exit apartmentDoor1 = new Exit(startApartment, commonRoom,"Turning the stainless-steel handle, you manage to slowly swing the door open and emerge into the hall beyond..","Turning the stainless-steel handle, you re-enter the apartment you awoke in..",true,"The door won't budge; an electronic lock holds it closed, awaiting an authorized keycard. Why is this on the inside?");
 		
@@ -134,8 +140,6 @@ public class Base {
 		Character[] characters = new Character[separatedInput.length];
 	
 		for(int i = 0;i < separatedInput.length;i++){
-			System.out.println("DEBUG: new for-loop iteration. i = " + i);
-			System.out.println(Item.isItem(separatedInput[i],separatedInput[i+1]));
 			if(separatedInput.length < i && Item.isItem(separatedInput[i],separatedInput[i + 1])){
 				items[i] = Item.getItem(separatedInput[i], separatedInput[i + 1]);
 				verbStrings[i] = null;
@@ -143,19 +147,15 @@ public class Base {
 				items[i] = null;
 				verbStrings[i] = null;
 				// when it finds a 2-word item it makes the next items element null and it then moves past it
-				System.out.println("DEBUG: two-word Item, i = " + i);
 			}else if(Item.isItem(separatedInput[i])){ // throws out-of-bounds w/ a 1 or a 2 when the for-loop uses "i <= seperatedInput.length"
 				items[i] = Item.getItem(separatedInput[i]);
 				verbStrings[i] = null;
-				System.out.println("DEBUG: one-word Item, i = " + i);
 			}else if(Character.isCharacter(separatedInput[i])){
 				characters[i] = Character.getCharacter(separatedInput[i]);
 				verbStrings[i] = null;
-				//System.out.println("DEBUG: character, i = " + i);
 			}
 			else{
 				verbStrings[i] = separatedInput[i];
-				System.out.println("DEBUG: verb, i = " + i);
 			}
 		}	
 		Verb.findAndExecuteAction(items, characters, verbStrings);
